2ac9dabaac0168dec5d3c109d60cfb07bae42ecf
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-05-09 16:36:53 +0300

    Update user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   name: String,
-  email: String
+  email: String,
+  favoriteBooks: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Book'
+  }]
 })
 
 const User = mongoose.model('User', userSchema)
